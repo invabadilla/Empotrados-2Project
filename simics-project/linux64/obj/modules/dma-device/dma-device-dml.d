1da873aa9f2efd8a6bc8bcae29c15a00baebcdd9
BMR0305 <bryanfmr0305@gmail.com>
2024-04-26 10:23:26 -0600

    Added simics project and devices

---
+++ simics-project/linux64/obj/modules/dma-device/dma-device-dml.d
@@ -0,0 +1,389 @@
+dma-device-dml.d dma-device-dml.o: dma-device-dml-cdep.c \
+ /usr/include/stdc-predef.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/devs/io-memory.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/types.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base-types.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/module-host-config.h \
+ /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h \
+ /usr/include/stdint.h /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/host-info.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/memory-transaction.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/attr-value.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/version.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/help-macros.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/strbuf.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/alloc.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/memory.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/pywrap.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/instrumentation-provider.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/bank-instrumentation.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/conf-object.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/sobject.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/vect.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/int-register.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/register-view.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/register-view-read-only.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/hashtab.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/bitcount.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/python.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/sim-get-class.h \
+ /home/usuario/Simics/simics-6.0.185/linux64/bin/dml/include/simics/dmllib.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/event.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/time.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/log.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/swabber.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/conf-object.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/build-id-6.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/build-id.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/dbuffer.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/frags.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/callbacks.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/arith.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/util/int128.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/duration.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/bigtime.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/local-time.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/cycle.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/clock.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/configuration.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/global-notifier.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/hap-producer.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/notifier.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/object-locks.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/sim-exception.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/obsolete/4_8.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/embed.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/obsolete/5.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/device-api.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/transaction.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/breakpoints.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/processor/types.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/map-target.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/direct-memory.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/devs/signal.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/devs/ram.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/base/cbdata.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/devs/translator.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/transaction.h \
+ /home/usuario/Simics/simics-6.0.185/src/include/simics/devs/memory-space.h
+
+/usr/include/stdc-predef.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/devs/io-memory.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/types.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base-types.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/module-host-config.h:
+
+/usr/include/stdio.h:
+
+/usr/include/x86_64-linux-gnu/bits/libc-header-start.h:
+
+/usr/include/features.h:
+
+/usr/include/x86_64-linux-gnu/sys/cdefs.h:
+
+/usr/include/x86_64-linux-gnu/bits/wordsize.h:
+
+/usr/include/x86_64-linux-gnu/bits/long-double.h:
+
+/usr/include/x86_64-linux-gnu/gnu/stubs.h:
+
+/usr/include/x86_64-linux-gnu/gnu/stubs-64.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h:
+
+/usr/include/x86_64-linux-gnu/bits/types.h:
+
+/usr/include/x86_64-linux-gnu/bits/timesize.h:
+
+/usr/include/x86_64-linux-gnu/bits/typesizes.h:
+
+/usr/include/x86_64-linux-gnu/bits/time64.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__FILE.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/FILE.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdio_lim.h:
+
+/usr/include/x86_64-linux-gnu/bits/sys_errlist.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/x86_64-linux-gnu/bits/waitflags.h:
+
+/usr/include/x86_64-linux-gnu/bits/waitstatus.h:
+
+/usr/include/x86_64-linux-gnu/bits/floatn.h:
+
+/usr/include/x86_64-linux-gnu/bits/floatn-common.h:
+
+/usr/include/x86_64-linux-gnu/sys/types.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/clock_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/clockid_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/time_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/timer_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdint-intn.h:
+
+/usr/include/endian.h:
+
+/usr/include/x86_64-linux-gnu/bits/endian.h:
+
+/usr/include/x86_64-linux-gnu/bits/endianness.h:
+
+/usr/include/x86_64-linux-gnu/bits/byteswap.h:
+
+/usr/include/x86_64-linux-gnu/bits/uintn-identity.h:
+
+/usr/include/x86_64-linux-gnu/sys/select.h:
+
+/usr/include/x86_64-linux-gnu/bits/select.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/sigset_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h:
+
+/usr/include/x86_64-linux-gnu/bits/pthreadtypes.h:
+
+/usr/include/x86_64-linux-gnu/bits/thread-shared-types.h:
+
+/usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h:
+
+/usr/include/x86_64-linux-gnu/bits/struct_mutex.h:
+
+/usr/include/x86_64-linux-gnu/bits/struct_rwlock.h:
+
+/usr/include/alloca.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdlib-float.h:
+
+/usr/include/string.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/locale_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__locale_t.h:
+
+/usr/include/strings.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h:
+
+/usr/include/stdint.h:
+
+/usr/include/x86_64-linux-gnu/bits/wchar.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdint-uintn.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/limits.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/syslimits.h:
+
+/usr/include/limits.h:
+
+/usr/include/x86_64-linux-gnu/bits/posix1_lim.h:
+
+/usr/include/x86_64-linux-gnu/bits/local_lim.h:
+
+/usr/include/linux/limits.h:
+
+/usr/include/x86_64-linux-gnu/bits/posix2_lim.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/host-info.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/memory-transaction.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/attr-value.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/version.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/help-macros.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/strbuf.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/alloc.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/memory.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/pywrap.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/instrumentation-provider.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/bank-instrumentation.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/conf-object.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/sobject.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/vect.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/int-register.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/register-view.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/register-view-read-only.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/hashtab.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/bitcount.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/python.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/sim-get-class.h:
+
+/home/usuario/Simics/simics-6.0.185/linux64/bin/dml/include/simics/dmllib.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/event.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/time.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/log.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/swabber.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/conf-object.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/build-id-6.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/build-id.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/dbuffer.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/frags.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/callbacks.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/arith.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/util/int128.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/duration.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/bigtime.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/local-time.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/cycle.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/clock.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/configuration.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/global-notifier.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/hap-producer.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/notifier.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/object-locks.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/sim-exception.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/obsolete/4_8.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/simulator/embed.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/obsolete/5.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/device-api.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/transaction.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/breakpoints.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/processor/types.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/map-target.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/direct-memory.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/devs/signal.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/devs/ram.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/base/cbdata.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/devs/translator.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/model-iface/transaction.h:
+
+/home/usuario/Simics/simics-6.0.185/src/include/simics/devs/memory-space.h:
